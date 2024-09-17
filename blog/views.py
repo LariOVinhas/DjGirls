c70a3d740754af4d0e49e7025537120514e67c8e
Larissa Pereira <larissapereira@MacBook-Pro-de-Larissa.local>
2024-09-17 20:29:44 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django. shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def portao(request):
+    return HttpResponse("voce chegou ao portão da casa")
+
+def sala(request):
+    return HttpResponse(' voce chepou na sala. Senta no sofa')
+
+def quarto(request):
+    return HttpResponse("Agora está no quarto, pode se deitar")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
